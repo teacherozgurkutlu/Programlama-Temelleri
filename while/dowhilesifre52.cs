d1aaf2f90d85c29b5800160f09d0f5048474c06c
teacherozgurkutlu <kutlu_ozgur@hotmail.com>
2019-02-22 11:12:21 +0300

    Add files via upload

---
+++ while/dowhilesifre52.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sifre;
+            do
+            {
+                Console.Write("Şifreyi gir :");
+                sifre = Console.ReadLine();
+            } while (sifre != "dilara123");
+            Console.Write("Programa hoş geldiniz!!!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ while/dowhilesifrebreak53.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sifre;
+            int a = 1;
+            do
+            {
+                Console.Write("Şifreyi gir :");
+                sifre = Console.ReadLine();
+                if (sifre == "dilara123")
+                {
+                    break;
+                }
+
+            } while (a == 1);
+            Console.Write("Programa hoş geldiniz!!!");
+            Console.ReadKey();
+
+        }
+    }
+}
