ffad191ab43aee0a8c3d28f40924b9372c36a2fe
teacherozgurkutlu <kutlu_ozgur@hotmail.com>
2019-02-22 11:34:58 +0300

    Add files via upload

---
+++ diziler/diziler54.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sebzeler = new string[5];
+
+            for (int i = 0; i <= 4; i++)
+            {
+                sebzeler[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(sebzeler[i]);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
